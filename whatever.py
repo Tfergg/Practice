1a4389c0e960e4b840f5c254c16c626a18466299
Mission Marcus <missionmarcus@gmail.com>
2022-05-11 22:49:14 -0500

    Added new file

---
+++ whatever.py
@@ -0,0 +1,30 @@
+x = "Cheer"
+# this is my variable 
+# print(x)
+
+num = 28
+num_3 = 9
+num = 10
+num_1 = num * num_3
+
+# print(num_1)
+
+this = [19, 20, 7, 1, 14, "LOL"]
+
+# for pizza in this:
+#     print("pizza")
+
+# for item in range(0, len(this)):
+#     # print(this)
+
+basket = [[2, 5, 9], [10.4, 3, 12.8], [8, 5, 3]]
+
+
+for eggs in range(0, len(basket)):
+    sum=0
+    for item in basket[eggs]:
+        sum = sum + item
+        
+    print(f"answer is {sum}")
+
+ 
